404aa0bd4f8afb1be8605d362afcb4e8f035c3b9
alaykabir <alaypratap23@gmail.com>
2024-01-21 16:00:31 +0530

    Onboarding-page-created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Onboarding from "./pages/Onboarding";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Onboarding />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
