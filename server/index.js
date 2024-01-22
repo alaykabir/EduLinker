a0a142288b9fb785c99fa8299f04a9af9602e30d
alaykabir <alaypratap23@gmail.com>
2024-01-22 23:33:35 +0530

    demo

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const url = `mongodb+srv://edulinker:8EHdU4u5zKEicZvD@cluster0.bqv3fyw.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to the database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. n${err}`);
+  });
 
 const app = express();
 
