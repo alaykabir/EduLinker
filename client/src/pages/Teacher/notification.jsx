ee76f6642c247bc3e0a0da21088b7896c36ccf72
Yash Kumar <yashkr2005@gmail.com>
2024-01-27 12:07:46 +0530

    Teacher-NavBar with Pages

---
+++ client/src/pages/Teacher/notification.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function notification() {
+    return (
+        <div>
+            request from student, will be displayed to faculty
+        </div>
+    )
+}
+
+export default notification
